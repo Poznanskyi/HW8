42dd9df8f8cc6ddd3a878040d75f6c8c566e41ea
Dmytro <114831210+Poznanskyi@users.noreply.github.com>
2022-12-14 14:16:06 +0200

    Update HW8.py

--- HW8.py
+++ HW8.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timedelta
 from collections import defaultdict
 
-# users = [
-#     {"Bob": (2001, 2, 8)},
-#     {"Alisa": (1999, 5, 6)},
-#     {"Mike": (1995, 8, 10)},
-#     ]
+
+users = [
+    {"Bob": (2001, 12, 22)},
+    {"Alisa": (1999, 12, 16)},
+    {"Mike": (1995, 12, 17)},
+]
 
 weekdays = {
     0: "Monday",
@@ -14,23 +15,23 @@ weekdays = {
     3: "Thursday",
     4: "Friday",
     5: "Saturday",
-    6: "Sunday"
+    6: "Sunday",
 }
 
 
 def happy_birthday(users, day=7):
-    current_date = datetime.now()
-    current_date = current_date.date()
+    current_date = datetime.now().date()
     end = current_date + timedelta(days=day)
 
     dct = defaultdict(list)
 
     for user in users:
         for name, birthday in user.items():
-            str_b = birthday.strftime('%Y %m %d').split()
+            str_b = [str(b) for b in birthday]
             str_b[0] = str(current_date.year)
             str_b = " ".join(str_b)
             birthday = datetime.strptime(str_b, '%Y %m %d')
+            birthday = datetime.strptime(str_b, "%Y %m %d")
             birthday = birthday.date()
 
             if current_date <= birthday < end:
@@ -43,3 +44,10 @@ def happy_birthday(users, day=7):
                     dct[birthday + timedelta(days=1)].append(name)
             else:
                 continue
+    return dct
+
+
+if __name__ == "__main__":
+    dt: datetime
+    for dt, pers in happy_birthday(users).items():
+        print(f"{dt.strftime('%A')} ({dt}) : {pers}")
